5cc911621aec5bcd29c2f7d3e039cbd28cabba9f
David Lane <david.lane@logistikgroup.com>
2013-04-22 17:08:46 +0100

    adding regex pattern

--- lib/scrape.rb
+++ lib/scrape.rb
@@ -4,5 +4,9 @@ require 'open-uri'
 url = "http://www.crossfit.com/cf-info/main_affil.htm"
 doc = Nokogiri::HTML(open(url))
 
+# Regex Pattern
+
+# /(<a href="(http:\/\/[A-Za-z0-9\/\.]+)" target="_blank">([\w\s]+)<\/a>([\w\s\-,&;]+)?<br>)/
+
 @affiliates_div = doc
 puts @affiliates_div
