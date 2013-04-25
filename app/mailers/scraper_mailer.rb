e48e77dbc38329b80abc33f155c45c3237b84cf2
David Lane <david.lane@logistikgroup.com>
2013-04-25 18:55:36 +0100

    Typo

--- app/mailers/scraper_mailer.rb
+++ app/mailers/scraper_mailer.rb
@@ -2,7 +2,7 @@ class ScraperMailer < ActionMailer::Base
   default :to => AdminUser.all.map(&:email),
           :from => "from@example.com"
   
-  def new_affiliate_added(new_affiliates)
+  def new_affiliates_added(new_affiliates)
     @affiliates = new_affiliates
     mail(:subject => "New Affiliate Added")
   end
