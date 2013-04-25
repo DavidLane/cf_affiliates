23ef725643e9518ce9fc60e9b61febe5b67ab21c
David Lane <david.lane@logistikgroup.com>
2013-04-25 18:52:46 +0100

    Changing the mailer to fire with an array

--- app/mailers/scraper_mailer.rb
+++ app/mailers/scraper_mailer.rb
@@ -2,8 +2,8 @@ class ScraperMailer < ActionMailer::Base
   default :to => AdminUser.all.map(&:email),
           :from => "from@example.com"
   
-  def new_affiliate_added(affiliate)
-    @affiliate = affiliate
+  def new_affiliate_added(new_affiliates)
+    @affiliates = new_affiliates
     mail(:subject => "New Affiliate Added")
   end
 end
