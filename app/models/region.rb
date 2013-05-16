c7acc392901da5bc52bd40ebfbf143acf6678295
David Lane <david.lane@logistikgroup.com>
2013-05-16 16:57:16 +0100

    Defaulting regions map

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,4 +1,10 @@
 class Region < ActiveRecord::Base
   has_many :affiliates
   attr_accessible :title, :coords_lat, :coords_long
+  
+  def has_coords?
+    if self.coords_lat.present? and self.coords_long.present?
+      return true
+    end
+  end
 end
